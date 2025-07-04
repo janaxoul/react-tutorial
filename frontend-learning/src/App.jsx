933248b354e8c75b456111efa386a7b98aff0b08
janaxoul <jxoul.k@gmail.com>
2025-07-04 09:55:24 +0530

    hooks

--- frontend-learning/src/App.jsx
+++ frontend-learning/src/App.jsx
@@ -1,26 +1,16 @@
 import { useState } from "react";
 
 function App() {
-  const [val, setVal] = useState([{name:"Mark", age:19}, {name:"Messi", age: 38}, {name:"Robert",age: 33}]);
+const[val, setVal]=useState(true);
   return (
-    <div className="p-5">
-      {val.map((elem, index) => (
-        <div key={index}>
-          <h1>{elem.name}</h1>
-          <h1>{elem.age}</h1>
-          <br />
-        </div>
-      ))}
-      <button
-        onClick={()=>{setVal(()=>{
-          val.map((item)=>{
-            item.name==="Messi"?{name:"Messi", age:37}: item
-          })
-        })}}
-        className="px-3 py-1 rounded-full bg-blue-400 text-white"
-      >
-        click
-      </button>
+    <div>
+      <h1>
+        {val?"Go on":"Don't Go"}
+      </h1>
+      <button onClick={()=>{
+          setVal((elem)=>!elem)
+        }}
+      className="px-3 py-1 rounded-full bg-blue-400 text-white">Change</button>
     </div>
   )
 }
---
+++ frontend-learning/src/Components/UseStateHooks.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UseStateHooks() {
+  return (
+    <div>UseStateHooks</div>
+  )
+}
+
+export default UseStateHooks
