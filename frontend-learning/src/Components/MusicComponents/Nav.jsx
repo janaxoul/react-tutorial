5c19eeb564b8d66d384cebeeddf2a5fbb1035d69
janaxoul <jxoul.k@gmail.com>
2025-07-04 18:35:20 +0530

    Music card demo

--- frontend-learning/src/Components/MusicComponents/Nav.jsx
+++ frontend-learning/src/Components/MusicComponents/Nav.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Nav({data}) {
-  return (
+return (
     <div className='w-full px-20 py-3 flex justify-between items center'>
         <h3 className='font-semibold text-orange-600'>Orange</h3>
         <div className='flex gap-3 p-2 px-4 bg-orange-600 text-sm text-white rounded-full'>
@@ -9,7 +9,7 @@ function Nav({data}) {
             <h4 className=' px-2 rounded-full bg-[#dadada8b]'>{data.filter(item => item.added).length}</h4>
         </div>
     </div>
-  )
+)
 }
 
 export default Nav
