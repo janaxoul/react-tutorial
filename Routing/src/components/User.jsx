27414d3b860e54d6f198843ce0228493c6c5384c
janaxoul <jxoul.k@gmail.com>
2025-07-06 11:37:45 +0530

    going for gaming session commit

--- Routing/src/components/User.jsx
+++ Routing/src/components/User.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function User() {
   return (
     <div className=" w-1/2 m-auto mt-10">
       <h1 className='text-5xl'>User</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi vero quasi laudantium omnis temporibus placeat voluptas odio a dolorem impedit!</p>
-      <button className='px-3 py-1 mt-2 rounded-md bg-sky-600'>User</button>
+      {/* <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi vero quasi laudantium omnis temporibus placeat voluptas odio a dolorem impedit!</p>
+      <button className='px-3 py-1 mt-2 rounded-md bg-sky-600'>User</button> */}
+      <div className="flex flex-col w-1/2 mt-5">
+        <Link className='p-3 bg-red-200 text-2xl mb-3 hover:bg-red-300 rounded-md' to='/user/john'>John</Link>
+        <Link className='p-3 bg-red-200 text-2xl mb-3 hover:bg-red-300 rounded-md' to='/user/robert'>Robert</Link>
+        <Link className='p-3 bg-red-200 text-2xl mb-3 hover:bg-red-300 rounded-md' to='/user/ryan'>Ryan</Link>
+      </div>
     </div>
   )
 }
--- Routing/src/utilities/Routing.jsx
+++ Routing/src/utilities/Routing.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import About from '../components/About'
 import Home from '../components/Home'
 import User from '../components/User'
+import UserDetail from '../components/UserDetail'
 function Routing() {
     return (
         <div>
             <Routes>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/user' element={<User/>}/>
+                <Route path='/user/:name' element={<UserDetail/>}/>
                 <Route path='/about' element={<About/>}/>
             </Routes>
         </div>
